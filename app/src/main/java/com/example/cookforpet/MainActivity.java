719d4872c0e9e80c39840c9ff1bdf36d0b814b12
Kyulang <rbfxod1223@gmail.com>
2022-01-04 14:27:37 +0900

    git commit

---
+++ app/src/main/java/com/example/cookforpet/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.cookforpet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onButton1Clicked(View v) {
+        Toast.makeText(this, "My refrigerator에 추가되었습니다.", Toast.LENGTH_LONG).show();
+    }
+}
